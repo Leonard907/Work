96d7ee4f7d2ebc9bf9f70c65ef3ccca3901f3d72
Leonard907 <ou_litu@163.com>
2019-11-27 15:25:37 +0000

    Last of CL

---
+++ CL/CL10/CL10regex.hs
@@ -0,0 +1,14 @@
+import CL9a
+  
+
+
+-- add your machines here using the following template
+
+machine :: FSM Int
+machine = mkFSM qs as ts ss fs where
+  qs = undefined
+  as = undefined
+  ts = undefined
+  ss = undefined
+  fs = undefined
+  
