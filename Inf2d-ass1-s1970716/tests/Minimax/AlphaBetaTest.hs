89de93c19a7c7a6de19871f91ac89188278cf793
Leonard907 <ou_litu@163.com>
2021-03-08 23:35:15 +0800

    RA CW1

---
+++ Inf2d-ass1-s1970716/tests/Minimax/AlphaBetaTest.hs
@@ -0,0 +1,73 @@
+{-
+    Module: PruneBreadthTest.
+
+    Used to test Part I.d.
+-}
+
+module AlphaBetaTest where
+
+import           Test.HUnit
+import           Test.Hspec
+import           Test.Hspec.Contrib.HUnit       ( fromHUnitTest )
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+
+import           Players.Minimax
+import           Types
+import           Util.Assertion
+import           Util.StateTreeInstances
+
+type TestTree = StateTree Int Action
+
+
+testTree1 :: TestTree
+testTree1 = StateTree
+    0
+    [ (Move (('a', 1), ('b', 2)), StateTree 3 ts)
+    , (Move (('b', 1), ('c', 2)), StateTree 4 ts)
+    , (Move (('c', 1), ('d', 2)), StateTree 0 ts)
+    ]
+  where
+    ts =
+        [ (Move (('d', 1), ('e', 2)), StateTree 5 [])
+        , (Move (('e', 1), ('f', 2)), StateTree 7 [])
+        , (Move (('f', 1), ('g', 2)), StateTree 1 [])
+        ]
+
+testTree2 :: TestTree
+testTree2 = StateTree
+    0
+    [ (Move (('a', 1), ('k', 2)), StateTree 3 ts1)
+    , (Move (('b', 1), ('c', 2)), StateTree 2 ts2)
+    , (Move (('c', 1), ('d', 2)), StateTree 2 ts3)
+    ]
+  where
+    ts1 =
+        [ (Move (('d', 1), ('e', 2)), StateTree 3 [])
+        , (Move (('e', 1), ('f', 2)), StateTree 12 [])
+        , (Move (('f', 1), ('g', 2)), StateTree 8 [])
+        ]
+    ts2 =
+        [ (Move (('d', 1), ('e', 2)), StateTree 3 [])
+        , (Move (('e', 1), ('f', 2)), StateTree 4 [])
+        , (Move (('f', 1), ('g', 2)), StateTree 6 [])
+        ]
+    ts3 =
+        [ (Move (('d', 1), ('e', 2)), StateTree 14 [])
+        , (Move (('e', 1), ('f', 2)), StateTree 5 [])
+        , (Move (('f', 1), ('g', 2)), StateTree 2 [])
+        ]
+
+test1 :: Test
+test1 = TestCase (assertEqual "Desired action" v (Move (('b', 1), ('c', 2))))
+    where v = minimaxABFromTree $ highFirst testTree1
+
+test2 :: Test
+test2 = TestCase (assertEqual "Desired action" v (Move (('a', 1), ('k', 2))))
+    where v = minimaxABFromTree $ highFirst testTree2
+
+abMinimax :: Spec
+abMinimax = fromHUnitTest $ TestList
+    [ TestLabel "testAlphaBetaCorrectness" test1
+    , TestLabel "testAlphaBetaCorrectness" test2
+    ]
